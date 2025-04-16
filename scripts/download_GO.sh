311079f4c307331f4510cdb88036df985432de6d
Jason Stajich <jasonstajich.phd@gmail.com>
2025-04-16 15:15:52 -0700

    add GO download

---
+++ scripts/download_GO.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -l
+mkdir -p lib
+pushd lib
+curl -O https://fungidb.org/common/downloads/release-68/AfumigatusA1163/gaf/FungiDB-68_AfumigatusA1163_Curated_GO.gaf.gz
+curl -O https://fungidb.org/common/downloads/release-68/AfumigatusA1163/gaf/FungiDB-68_AfumigatusA1163_GO.gaf.gz
+curl -O https://fungidb.org/common/downloads/release-68/AfumigatusAf293/gaf/FungiDB-68_AfumigatusAf293_Curated_GO.gaf.gz
+curl -O https://fungidb.org/common/downloads/release-68/AfumigatusAf293/gaf/FungiDB-68_AfumigatusAf293_GO.gaf.gz
+
