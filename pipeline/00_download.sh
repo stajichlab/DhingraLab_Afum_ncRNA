eeca22135f350baa1bdb8969810538b2de1fbe42
Jason Stajich <jasonstajich.phd@gmail.com>
2025-04-16 15:26:40 -0700

    add download GO terms in pipeline step

--- pipeline/00_download.sh
+++ pipeline/00_download.sh
@@ -13,16 +13,27 @@ do
     MRNA=${SPECIES}_mRNA.fasta
     GENOME=${SPECIES}_Genome.fasta
     GFF=${SPECIES}_Genes.gff3
-    
+    GO=${SPECIES}_GO.gaf.gz
+    GOCURATED=${SPECIES}_Curated_GO.gaf.gz
+
     if [ ! -f $DB/$MRNA ]; then
-	curl -o $DB/$MRNA $URL/$SPECIES/fasta/data/FungiDB-${VERSION}_${SPECIES}_AnnotatedTranscripts.fasta 
+        curl -o $DB/$MRNA $URL/$SPECIES/fasta/data/FungiDB-${VERSION}_${SPECIES}_AnnotatedTranscripts.fasta 
     fi
     
     if [ ! -f $DB/$GENOME ]; then
-	curl -o $DB/$GENOME $URL/$SPECIES/fasta/data/FungiDB-${VERSION}_${SPECIES}_Genome.fasta
+        curl -o $DB/$GENOME $URL/$SPECIES/fasta/data/FungiDB-${VERSION}_${SPECIES}_Genome.fasta
     fi
     
     if [ ! -f $DB/$GFF ]; then
-	curl -o $DB/$GFF $URL/$SPECIES/gff/data/FungiDB-${VERSION}_${SPECIES}.gff
+        curl -o $DB/$GFF $URL/$SPECIES/gff/data/FungiDB-${VERSION}_${SPECIES}.gff
     fi
+    if [ ! -f $DB/${GO} ]; then
+        curl -o $DB/$GO $URL/$SPECIES/gaf/data/FungiDB-${VERSION}_${GO}
+    fi
+
+    if [ ! -f $DB/${GOCURATED} ]; then
+        curl -o $DB/${GOCURATED} $URL/$SPECIES/gaf/data/FungiDB-${VERSION}_${GOCURATED}
+    fi
+
+
 done
